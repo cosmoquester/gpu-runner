178ca2968098a4608453785c7796751d490bb3c0
Sangjun Park <cosmoquester@gmail.com>
2024-08-02 17:25:04 +0900

    chore: Setup Project

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="python3-template",
+    name="gpu-runner",
     version="0.0.1",
-    description="This repository is template for my python project.",
+    description="Run a command requiring GPU resources",
     python_requires='>=3.7',
     install_requires=[],
-    url="https://github.com/cosmoquester/python3-template.git",
+    url="https://github.com/cosmoquester/gpu-runner.git",
     author="Park Sangjun",
     packages=find_packages(exclude=["tests"]),
 )
