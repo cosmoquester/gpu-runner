19c5de67f094d1b30bc6912f3a9e0ffa7eb0133f
Sangjun Park <cosmoquester@gmail.com>
2024-08-02 19:32:22 +0900

    chore: Add __version__

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import find_packages, setup
+from grun import __version__
 
 setup(
     name="gpu-runner",
-    version="0.0.1",
+    version=__version__,
     description="Run a command requiring GPU resources",
     python_requires=">=3.7",
     install_requires=[
